75932b72a96ff8f0fc5f716fc8ae5e58a86f000d
Ngô Sỹ Trường <truongfatty94@gmai.com>
2016-04-21 14:42:27 +0700

    Initial Commit

---
+++ CampFire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CampFire
+//
+//  Created by Ngô Sỹ Trường on 4/21/16.
+//  Copyright © 2016 ngotruong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
