07f66b846d8e03c78755bfaf62ce36c13bc13ed1
Ngô Sỹ Trường <truongfatty94@gmai.com>
2016-04-28 15:41:06 +0700

    first commit

--- CampFire/ViewController.m
+++ CampFire/ViewController.m
@@ -9,19 +9,38 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIImageView* fire;
+}
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void) loadView {
+    [super loadView];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    self.view.backgroundColor = [UIColor blackColor];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    [self burnFire];
+}
+- (void) burnFire {
+    fire = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    NSMutableArray* images = [[NSMutableArray alloc] initWithCapacity:17];
+    for (int i = 1; i < 18; i++) {
+        NSString* fileName;
+        if (i < 10) {
+            fileName = [NSString stringWithFormat:@"campFire0%d.gif",i];
+        } else {
+            fileName = [NSString stringWithFormat:@"campFire%d.gif",i];
+        }
+        [images addObject:[UIImage imageNamed:fileName]];
+    }
+    
+    fire.animationImages = images;
+    fire.animationDuration = 1;
+    fire.animationRepeatCount = 0;
+    [self.view addSubview:fire];
+    [fire startAnimating];
 }
 
 @end
